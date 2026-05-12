cdf31cffb25af81aa6be154013aef66297a60032
Usama Bilal <ub45@drexel.edu>
2026-05-12 17:52:39 -0400

    updated to fix error in list.files, add a check for no files, and use .by instead of gorup_By

--- 01_import_data.R
+++ 01_import_data.R
@@ -3,22 +3,26 @@ library(arrow)
 
 # READ FILES
 files <- list.files(
-  "2026_panama_reproducibility",
-  pattern = "*.parquet",
+  path = "2026_panama_reproducibility",
+  pattern = "\\.parquet$",
   full.names = TRUE
 )
+
+if (length(files) == 0) {
+  stop("No .parquet files found in '2026_panama_reproducibility'.", call. = FALSE)
+}
+
 df <- map_dfr(files, read_parquet)
 
 df <- df |>
-  group_by(across(c(sim_id, date))) |>
   summarize(
     deaths = sum(deaths),
     cardio = sum(cardio),
     respdisease = sum(respdisease),
     respinf = sum(respinf),
     temp = mean(temp),
-    pm25 = mean(pm25)
+    pm25 = mean(pm25),
+    .by = c(sim_id, date)
   ) |>
-  ungroup() |>
   mutate(strata = paste0(year(date), ":", month(date), ":", wday(date))) |>
   mutate(strata = as_factor(strata))
